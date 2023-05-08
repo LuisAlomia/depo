2c5307b04491fd814b5c3bf64af5aac3400a704e
Luis Alomia <luisfidel44@gmail.com>
2023-05-07 21:50:10 -0500

    text(vitest): add configuration vitest

---
+++ test/test.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "vitest";
+
+describe("Should return 2", () => {
+  test("should ", () => {
+    const result: number = 1 + 1;
+
+    expect(result).toBe(2);
+  });
+});
